10229019842fb3c18f40b5cda027ec49a79722f2
adante77 <76264477+adante77@users.noreply.github.com>
2021-12-29 17:51:37 +0330

    Update ipfs.service.ts

--- src/services/ipfs.service.ts
+++ src/services/ipfs.service.ts
@@ -21,7 +21,45 @@ export class ipfsService{
         }
        
         const { cid } = await this.ipfs.add(file)
+
+
+        // const stream = this.ipfs.cat('QmaQdLVhfspQmnfv4oR86cL7VdWsY2K8q6h6EQNoUmSqBi')
+        // let data = ''
+
+        // for await (const chunk of stream) {
+        // // chunks of data are returned as a Buffer, convert it back to a string
+        // data += chunk.toString()
+        // }
+
+        // console.log(data)
+
         return cid.toString()
+
     }
+
+    public async getFile(cid: string):Promise<string>{
+        if(!this.ready){
+            const ipfs = await IPFS.create()
+            this.ipfs = ipfs
+            this.ready = true
+        }
+       
+
+
+        const stream = this.ipfs.cat(cid)
+        let data = ''
+
+        for await (const chunk of stream) {
+        // chunks of data are returned as a Buffer, convert it back to a string
+        data += chunk.toString()
+        }
+
+
+        return data
+
+    }
+
+
+
 
 }
